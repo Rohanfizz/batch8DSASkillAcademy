f269ccef051dc69553b3768ff67b8d63498dabc9
Rohanfizz <rohans0501@gmail.com>
2023-05-27 20:56:46 +0530

    iterative1

--- iterative1/pattern4.js
+++ iterative1/pattern4.js
@@ -4,7 +4,7 @@ let n = 150;
 for(let row = 1;row<=n;row++){
     let thisRow = "";
     for(let col = 1;col<=n;col++){
-        if(row == col) thisRow+="\\";
+        if(row == col) thisRow+="*";
         else thisRow += "o";
     }
     console.log(thisRow);
