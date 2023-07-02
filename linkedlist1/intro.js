b3dd2c448f7001aaf6da53cc216ba1fd30dce6cf
Rohanfizz <rohans0501@gmail.com>
2023-07-02 19:05:04 +0530

    linkedlist1

--- linkedlist1/intro.js
+++ linkedlist1/intro.js
@@ -23,10 +23,43 @@ var LinkedList = /** @class */ (function () {
                 this.tail = nn;
                 this.size++;
             }
-            else { //list  is not empty
+            else {
+                //list  is not empty
                 this.tail.next = nn;
                 this.tail = this.tail.next;
                 this.size++;
+            }
+        };
+        this.addFirst = function (x) {
+            var nn = new ListNode(x);
+            if (this.size == 0) { //list is empty
+                this.head = nn;
+                this.tail = nn;
+                this.size++;
+            }
+            else {
+                nn.next = this.head;
+                this.head = nn;
+                this.size++;
+            }
+        };
+        this.removeFirst = function () {
+            if (this.size == 0) {
+                console.log("List is already empty!");
+                return null;
+            }
+            else if (this.size == 1) {
+                var res = this.head.val;
+                this.head = null;
+                this.tail = null;
+                this.size--;
+                return res;
+            }
+            else {
+                var res = this.head.val;
+                this.head = this.head.next;
+                this.size--;
+                return res;
             }
         };
         this.head = null;
@@ -36,9 +69,9 @@ var LinkedList = /** @class */ (function () {
     return LinkedList;
 }());
 var ll = new LinkedList();
-ll.addLast(100);
-ll.addLast(20);
-ll.addLast(30);
-ll.addLast(40);
-ll.addLast(50);
+ll.addFirst(5);
+ll.addFirst(4);
+ll.addFirst(3);
+ll.removeFirst();
+ll.addLast(3);
 ll.display();
--- linkedlist1/intro.ts
+++ linkedlist1/intro.ts
@@ -1,13 +1,13 @@
-class  ListNode{
-    val : number;
-    next : ListNode | null;
-    constructor(x:number){
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(x: number) {
         this.val = x;
         this.next = null;
     }
 }
 
-class LinkedList{
+class LinkedList {
     head: ListNode | null;
     tail: ListNode | null;
     size: number;
@@ -18,37 +18,67 @@ class LinkedList{
         this.size = 0;
     }
 
-    display = function():void{
+    display = function (): void {
         let temp = this.head;
-        let s="";
-        while(temp != null){
-            s = s + temp.val +" -> ";
+        let s = "";
+        while (temp != null) {
+            s = s + temp.val + " -> ";
             temp = temp.next;
         }
         console.log(s);
-    }
+    };
 
-    addLast = function( x : number ) : void {
-        let nn : ListNode = new ListNode(x);
+    addLast = function (x: number): void {
+        let nn: ListNode = new ListNode(x);
 
-        if(this.size == 0){
+        if (this.size == 0) {
             this.head = nn;
-            this.tail  = nn;
+            this.tail = nn;
             this.size++;
-        }else{//list  is not empty
+        } else {
+            //list  is not empty
             this.tail.next = nn;
             this.tail = this.tail.next;
             this.size++;
         }
+    };
+
+    addFirst = function (x: number): void {
+        let nn = new ListNode(x);
+        if(this.size == 0){//list is empty
+            this.head = nn;
+            this.tail = nn;
+            this.size++;
+        }else{
+            nn.next = this.head;
+            this.head = nn;
+            this.size++;
+        }
+    };
+
+    removeFirst = function () : number | null{
+        if(this.size == 0){
+            console.log("List is already empty!");
+            return null;
+        }else if(this.size == 1){
+            let res = this.head.val;
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return res;
+        }else{
+            let res = this.head.val;
+            this.head = this.head.next;
+            this.size--;
+            return res;
+        }
     }
-
-
 }
 
 let ll = new LinkedList();
-ll.addLast(100);
-ll.addLast(20);
-ll.addLast(30);
-ll.addLast(40);
-ll.addLast(50);
+ll.addFirst(5);
+ll.addFirst(4);
+ll.addFirst(3);
+ll.removeFirst();
+ll.addLast(3);
 ll.display();
